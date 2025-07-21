44edc252f85dd54731521a929d0e4808eb9afe9f
Ketansingh0 <ketansingh831856@gmail.com>
2025-07-21 13:16:35 +0530

    layout done

--- Vakya/src/components/chatSection/ChatSection.jsx
+++ Vakya/src/components/chatSection/ChatSection.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./ChatSection.css";
+import { TbSend2 } from "react-icons/tb";
 import Darkmode from "../Darkmode/Darkmode";
 function ChatSection() {
   return (
     <div className="chatsection">
-      <Darkmode />
+      <div className="topsection">
+        <div className="headings">
+          <span>Hello Adarsh</span>
+          <span>I'm Your own Assistant</span>
+          <span>How can I help you...?</span>
+        </div>
+      </div>
+      <div className="bottomsection">
+        <input type="text" placeholder="Enter a prompt" id="" />
+        <button id="sendbtn">
+          <TbSend2 />
+        </button>
+        <Darkmode />
+      </div>
     </div>
   );
 }
